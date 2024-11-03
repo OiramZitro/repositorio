114d172877b40d78d350db9a914b16207848255c
mario <mario.ortiz@gmail.com>
2024-11-03 12:24:48 +0100

    primera version del juego del ahorcado implementado en JAVA

---
+++ src/JuegoCompletado.java
@@ -0,0 +1,10 @@
+public class JuegoCompletado {
+    public static boolean juegoCompletado(String palabra, String letrasAdivinadas) {
+        for (char letra : palabra.toCharArray()) {
+            if (letrasAdivinadas.indexOf(letra) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
